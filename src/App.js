cfd57865bf541b317d9885ec1bab1da3a1681a3c
Alisa <alisa.220408@gmail.com>
2021-10-06 16:02:37 +0400

    Application skeleton commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import './App.css';
+import Notes from './Notes';
+import NotesHeader from "./NotesHeader";
+import NoteForm from "./NoteForm";
+import {useState} from "react";
 
 function App() {
+  const [notesTitle, setNotesTitle] = useState("")
+  const [notes, setNotes] = useState([
+    {
+      id: Math.random(),
+      title: "First Note Title",
+      note: "My first note",
+    },
+    {
+      id: Math.random(),
+      title: "The other note's title",
+      note: "My note",
+    },
+    {
+      id: Math.random(),
+      title: "One more Title",
+      note: "And one more note",
+    }
+  ]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <NotesHeader onSave={(notesTitle) => {
+         console.log("Save or not, that's a question!")
+      }}/>
+      <NoteForm onAdd={(title, note) => {
+          setNotes([
+            ...notes,
+            {
+              id: Math.random(),
+              title: title,
+              note: note
+            }
+          ])
+      }}/>
+      <Notes notes={notes} onSave={() => {
+         console.log("Save or not, that's a question!")
+      }}/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import reactDom from 'react-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-//import reportWebVitals from './reportWebVitals';
+import App from './App';
 
-
-function App() {
-  return React.createElement("div", {}, "Hello Disqo");
-}
-
-
-reactDom.render(App(), document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
